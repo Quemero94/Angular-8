4651eb82e278f0df8a10da37f13272fee088ab79
Quemero94 <matiaserco@gmail.com>
2019-08-30 17:03:46 -0300

    first commit

---
+++ 2-TypeScript y ES6/proyecto-inicial/app.js
@@ -0,0 +1,20 @@
+"use strict";
+//Tipos de variables
+var nombre = "Peter";
+var numero = 123;
+var booleano = true;
+var hoy = new Date();
+hoy = new Date("2020-10-10");
+var cualquier;
+cualquier = nombre;
+cualquier = numero;
+cualquier = booleano;
+cualquier = hoy;
+var spiderman = {
+    nombre: "Peter",
+    edad: 20
+};
+spiderman = {
+    nombre: "Juan",
+    edad: 30
+};
---
+++ 2-TypeScript y ES6/proyecto-inicial/app.ts
@@ -0,0 +1,25 @@
+//Tipos de variables
+
+let nombre: string = "Peter";
+let numero: number = 123;
+let booleano: boolean = true;
+
+let hoy: Date = new Date();
+hoy = new Date("2020-10-10");
+
+let cualquier: any;
+
+cualquier = nombre;
+cualquier = numero;
+cualquier = booleano;
+cualquier = hoy;
+
+let spiderman = {
+  nombre: "Peter",
+  edad: 20
+};
+
+spiderman = {
+  nombre: "Juan",
+  edad: 30
+};
